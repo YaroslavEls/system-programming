0049a26f9883ffa537b3205d0e391adff5edef2e
Ярослав Терещенко <yarter003@gmail.com>
2022-06-23 00:09:09 +0300

    lab 5 fix

--- lab5/lab5.asm
+++ lab5/lab5.asm
@@ -3,10 +3,10 @@ option casemap:none
 include \masm32\include\masm32rt.inc
 .data?
 buff db 128 dup(?)
-.data                       ; res -13, 2, 1, -4, 2
-arrA dd 8,6,2,6,4
-arrC dd 6,9,5,3,8
-arrD dd 6,-9,5,-3,2
+.data                       ; res -13, 2, 1, -4, 2, 11
+arrA dd 8,6,2,6,4,-6
+arrC dd 6,9,5,3,8,-8
+arrD dd 6,-9,5,-3,2,4
 tmp1 dd ?
 tmp2 dd ?
 MsgBoxCaption db "Lab5. Yaroslav Tereshchenko IP-04",0
@@ -17,12 +17,14 @@ Main:
 Calculate:
     mov eax, arrC[ebx*4]    ; c/d
     mov edx, 0
+    cdq
     idiv arrD[ebx*4]
     mov tmp1, eax
     mov eax, 3              ; 3*a/2
     imul arrA[ebx*4]
     mov edx, 0
     mov tmp2, 2
+    cdq
     idiv tmp2
     add eax, tmp1           ; tmp2+tmp1 = (3*a/2)+(c/d)
     mov tmp1, eax
@@ -32,6 +34,7 @@ Calculate:
     mov tmp2, eax                        
     mov eax, tmp1           ; tmp1/tmp2 = ((3*a/2)+(c/d))/(c-a+1)
     mov edx, 0
+    cdq
     idiv tmp2
     mov tmp1, eax           ; check for odd/even
     mov tmp2, 2
@@ -46,7 +49,7 @@ Output:
     invoke wsprintf, addr buff, addr form, arrA[ebx*4], arrC[ebx*4], arrD[ebx*4], eax
     invoke MessageBox, 0, addr buff, offset MsgBoxCaption, 0
     inc ebx
-    cmp ebx, 5
+    cmp ebx, 6
     jl Calculate
 Exit:
     invoke ExitProcess, 0
